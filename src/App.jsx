ce1f47f0b66817950c6e27e51122b710d6bea0f2
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-08-14 09:32:17 +0200

    Adicionei funcionalidade para scroll pra cima no código.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import "./style/global.css";
 import Footer from './components/layout/Footer/Footer';
 import Header from './components/layout/Header/Header';
 import Rotas from './routes/Rotas';
+import ScrollCima from './components/layout/scroll/ScrollCima';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="imperial_container">
         <Header />
-        <main className="conteudo">
+        <ScrollCima />
+        <main id="conteudo">
           <Rotas />
           <Footer />
         </main>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,6 +7,20 @@ const Home = () => {
         <ContainerSessao className={stlye.container_home}>
             <p>Home</p>
             <Hero />
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto dicta temporibus nemo repellat aperiam sequi dolorum nobis labore ipsam molestias? Et culpa omnis rem fuga magnam, deserunt distinctio corrupti dolorum.
+                Totam, pariatur dolorum perspiciatis minima tempore minus inventore ab. Reiciendis assumenda expedita inventore dignissimos alias nisi eaque harum exercitationem voluptas praesentium aspernatur, temporibus cumque sapiente quod laudantium officia, recusandae tenetur?
+                Tempore quos cumque numquam odio doloribus, repellat eaque, aliquam alias officiis animi, ullam inventore recusandae autem aperiam delectus impedit? Iure cumque aliquid corrupti esse aliquam numquam ducimus ad, officia quas?
+                Quas ea voluptatum illum! Dolores excepturi ullam recusandae maiores cupiditate autem cum quidem beatae molestias fugit esse, necessitatibus nisi? Culpa obcaecati deleniti dolorem non libero atque ipsam perspiciatis dignissimos numquam!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto dicta temporibus nemo repellat aperiam sequi dolorum nobis labore ipsam molestias? Et culpa omnis rem fuga magnam, deserunt distinctio corrupti dolorum.
+                Totam, pariatur dolorum perspiciatis minima tempore minus inventore ab. Reiciendis assumenda expedita inventore dignissimos alias nisi eaque harum exercitationem voluptas praesentium aspernatur, temporibus cumque sapiente quod laudantium officia, recusandae tenetur?
+                Tempore quos cumque numquam odio doloribus, repellat eaque, aliquam alias officiis animi, ullam inventore recusandae autem aperiam delectus impedit? Iure cumque aliquid corrupti esse aliquam numquam ducimus ad, officia quas?
+                Quas ea voluptatum illum! Dolores excepturi ullam recusandae maiores cupiditate autem cum quidem beatae molestias fugit esse, necessitatibus nisi? Culpa obcaecati deleniti dolorem non libero atque ipsam perspiciatis dignissimos numquam!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto dicta temporibus nemo repellat aperiam sequi dolorum nobis labore ipsam molestias? Et culpa omnis rem fuga magnam, deserunt distinctio corrupti dolorum.
+                Totam, pariatur dolorum perspiciatis minima tempore minus inventore ab. Reiciendis assumenda expedita inventore dignissimos alias nisi eaque harum exercitationem voluptas praesentium aspernatur, temporibus cumque sapiente quod laudantium officia, recusandae tenetur?
+                Tempore quos cumque numquam odio doloribus, repellat eaque, aliquam alias officiis animi, ullam inventore recusandae autem aperiam delectus impedit? Iure cumque aliquid corrupti esse aliquam numquam ducimus ad, officia quas?
+                Quas ea voluptatum illum! Dolores excepturi ullam recusandae maiores cupiditate autem cum quidem beatae molestias fugit esse, necessitatibus nisi? Culpa obcaecati deleniti dolorem non libero atque ipsam perspiciatis dignissimos numquam!
+            </p>
         </ContainerSessao>
     )
 }
